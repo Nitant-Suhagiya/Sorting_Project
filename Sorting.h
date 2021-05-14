592aaa76d576134612a159a20d4cdb4b92c96a58
Nitant Suhagiya <71947520+Nitant-Suhagiya@users.noreply.github.com>
2021-05-14 17:11:56 -0600

    Create Sorting.h

---
+++ Sorting.h
@@ -0,0 +1,11 @@
+#pragma once
+
+void swap(int *small, int *big);
+void SelectionSort(int arr[], int n);
+void BubbleSort(int arr[], int n);
+void InsertionSort(int arr[], int n);
+void Merge(int arr[], int FirstS, int FirstE, int SecondE);
+void MergeSort(int arr[], int l, int r);
+void Heap(int arr[], int size, int root);
+void HeapSort(int arr[], int size);
+void PrintArray(int arr[], int n);
