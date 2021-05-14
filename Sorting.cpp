c3c231d8cc2d9443e63245e10f476e9ec0b204ac
Nitant Suhagiya <71947520+Nitant-Suhagiya@users.noreply.github.com>
2021-05-14 17:15:38 -0600

    Updated Sorting.cpp
    Sorting Algorithms

--- Sorting.cpp
+++ Sorting.cpp
@@ -14,19 +14,19 @@ void swap(int *small, int *big)
 
 void SelectionSort(int arr[], int n)
 {
-	int min_ind;
+	int min_index;
 	for (int i = 0; i < n - 1; i++)
 	{
-		min_ind = i;
+		min_index = i;
 		for (int j = i + 1; j < n; j++)
 		{
-			if (arr[j] < arr[min_ind])
+			if (arr[j] < arr[min_index])
 			{
-				min_ind = j;
+				min_index = j;
 			}
 		}
 
-		swap(&arr[min_ind], &arr[i]);
+		swap(&arr[min_index], &arr[i]);
 
 	}
 }
